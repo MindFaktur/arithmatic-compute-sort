49f18660c34c3389ec57290f3c5210497097a4aa
Vishwanath <vishwanathiti828@gmail.com>
2021-11-07 09:41:07 +0530

    MODIFIED uc1.sh

--- uc1.sh
+++ uc1.sh
@@ -4,7 +4,7 @@ echo "This is a program to take three inputs from the user"
 
 read -p "Enter three numbers with a space between each: " a b c
 
-echo $a
-echo $b
-echo $c
+echo "1st number is $a"
+echo "1st number is $b"
+echo "1st number is $c"
 
