2c9fa2da29d0ad8139d04d196b49cbe238e25fbc
Vishwanath <vishwanathiti828@gmail.com>
2021-11-07 10:36:44 +0530

    ADDED uc6.sh

---
+++ uc6.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+declare -A computations
+read -p "Enter 3 numbers with space in between each " a b c
+
+computations[uc2]=`awk "BEGIN {print $a+($b*$c)}"`
+computations[uc3]=`awk "BEGIN {print ($a*$b)+$c}"`
+computations[uc4]=`awk "BEGIN {print $c+($a/$b)}"`
+computations[uc5]=`awk "BEGIN {print ($a % $b)+$c}"`
+
+echo ${computations[@]}
+
